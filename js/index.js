0b6f65f0376708a63540e6bf62435c87bc4cf57b
monikatamsoy <ssumugan007@gmail.com>
2018-04-24 08:25:35 +0530

    add clock

--- js/index.js
+++ js/index.js
@@ -20,6 +20,24 @@ function getAngle() {
             var angle = Math.abs(hrsA - minsA)
             var reqAngle = (angle > 180) ? (360 - angle) : angle
             result.innerHTML = reqAngle + "<sup>o</sup>"
+            setTime(hrsA, minsA)
         }
     }
 }
+
+function setTime(hrsA, minsA) {
+    var minutes = document.getElementById("minutes")
+    var hours = document.getElementById("hours")
+    var date = new Date()
+    var minsAngle = ((minsA == 0) ? 0 : minsA) || date.getMinutes() * 360 / 60
+    var hrsAngle = hrsA || ((date.getHours() > 12) ? (date.getHours()-12) : date.getHours() ) * 360 / 12 + minsAngle/12
+    hours.style.transform = "rotate(" + hrsAngle + "deg)"
+    minutes.style.transform = "rotate(" + minsAngle + "deg)"
+}
+setTimeout(function() {
+   setTime()
+}, 20)
+
+setInterval(function() {
+    setTime()
+}, 10000)
